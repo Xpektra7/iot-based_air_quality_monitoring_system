e8f19133a6c81e22610ce275b65d44bb6370a408
Xpektra <xpektra7@gmail.com>
2026-01-11 23:46:48 +0100

    Add LED and resistor components to the circuit diagram; update main logic for LED control based on gas readings

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,44 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+const int greenPin = 16;
+const int yellowPin = 15;
+const int redPin = 12;
+
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  pinMode(greenPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
 }
 
 void loop() {
-  int value13 = analogRead(13);
-  int value14 = analogRead(14);
+  int gasValue = analogRead(13);
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
 
   Serial.print("Aout: ");
-  Serial.print(value13);
-  Serial.print("\tDout: ");
-  Serial.print(value14);
+  Serial.print(gasValue);
   Serial.print("\tTemp: ");
   Serial.print(temperature);
   Serial.print("\tHum: ");
   Serial.println(humidity);
+
+  if(gasValue > 3000 ){
+    digitalWrite(redPin,HIGH);
+    digitalWrite(yellowPin,LOW);
+    digitalWrite(greenPin,LOW);
+  }
+  else if(gasValue > 2000){
+    digitalWrite(redPin,LOW);
+    digitalWrite(yellowPin,HIGH);
+    digitalWrite(greenPin,LOW);
+  } else {
+    digitalWrite(redPin,LOW);
+    digitalWrite(yellowPin,LOW);
+    digitalWrite(greenPin,HIGH);
+  }
 
   delay(500);
 }
